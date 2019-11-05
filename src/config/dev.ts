e8f48181fbeda3f1f12629ec30e8d683f62a740c
Simon Radner <sr@vivid-planet.com>
2019-11-05 17:33:44 +0100

    Remove test and production config, make config ENV-injectable

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,16 +1,13 @@
-import { IConfig } from "app/config/index";
-import test from "app/config/test";
+import { defaultConfig, IConfig } from "app/config/index";
 
 declare const CONFIG_DEV_LOCAL_EXISTS: boolean;
 
 let config: IConfig = {
-    ...test,
-    apiUrl: "http://dev.example.com",
+    ...defaultConfig,
+    // override default-values for development here
 };
 
-if (process.env.NODE_ENV !== "production") {
-    const devLocal: IConfig = CONFIG_DEV_LOCAL_EXISTS ? require("app/config/dev.local").default : {};
-    config = { ...config, ...devLocal };
-}
+const devLocal: IConfig = CONFIG_DEV_LOCAL_EXISTS ? require("app/config/dev.local").default : {};
+config = { ...config, ...devLocal };
 
 export default config;
