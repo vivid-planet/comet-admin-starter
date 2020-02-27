54e1bf636ec4f989687e8fe7bfc65f857a5aa0b7
Niko Sams <ns@vivid-planet.com>
2020-02-27 14:53:42 +0100

    Do proper linting, not just jslint

--- lint.sh
+++ lint.sh
@@ -4,8 +4,10 @@ set -e
 for SUBPROJECT in admin; do
 
     if [ -n "$INSTALL_DEPS" ]; then 
+        echo "installing dependencies for $SUBPROJECT..."
         docker-compose run --rm --no-deps $SUBPROJECT bash -c "npm install"
     fi
 
+    echo "linting $SUBPROJECT..."
     docker-compose run --rm --no-deps $SUBPROJECT bash -c "npm run lint"
 done
